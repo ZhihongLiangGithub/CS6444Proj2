96744b8defbed052e244a546c56ed82819b3ef77
Zhihong <zhihongliang@gwmail.gwu.edu>
2017-06-30 08:35:46 -0400

    corpus update

--- build_corpus.R
+++ build_corpus.R
@@ -1,5 +1,7 @@
-load("prolific.RData")
 library(tm)
+# central people
+cp <-
+  "david.forster@enron.com|jeff.dasovich@enron.com|tana.jones@enron.com|veronica.espinoza@enron.com|steven.kean@enron.com"
 maildir <-
   "C:/Users/Liang/Desktop/BigData/enron_mail_20150507/maildir"
 cordir <- "C:/Users/Liang/Desktop/BigData/corpus"
@@ -8,9 +10,24 @@ f <- function(path) {
   lines <- readLines(path)
   #extract the sender
   sender <- gsub("From: ", "", lines[grepl("^From: .*", lines)][1])
-  if (sender %in% prolific$sender) {
-    print(path)
+  #extract the receiver(s)
+  start <- grep("^To:.*", lines)[1]
+  end <- grep("^Subject:.*", lines)[1] - 1
+  receiver <- character()
+  if (is.na(start)) {
+    receiver <- NA
+  } else{
+    if (start > end) {
+      receiver <- NA
+    } else{
+      receiver <- gsub("\t", "", lines[start:end])
+      receiver <- gsub("To: ", "", receiver)
+      receiver <- paste(receiver, collapse = "")
+    }
+  }
+  if (grepl(cp, sender) || grepl(cp, receiver)) {
     count <<- count + 1
+    print(count)
     file.copy(path, paste(c(cordir, count), collapse = "/"))
   }
 }
@@ -19,5 +36,5 @@ files <- list.files(path = maildir,
                     recursive = T)
 lapply(files, f)
 vc <- VCorpus(DirSource(cordir))
-save(vc, file = "corpus.RData")
 
+save(vc, file = "corpus.RData")
